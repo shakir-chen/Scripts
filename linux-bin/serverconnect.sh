8ce96298ca31180b6de1e2dbaec37753ba4fc936
shakir-chen <chenxuanqi93@gmail.com>
2016-08-26 01:16:32 +0800

    serverconnect up

--- linux-bin/serverconnect.sh
+++ linux-bin/serverconnect.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-servername="xuanqi@"$1".ece.ust.hk"
+echo $1
+if [ "$1" = "lab" ];
+then
+    echo "here"
+    servername="shakir@143.89.135.225 -p 2222"
+else
+    servername="xuanqi@"$1".ece.ust.hk"
+fi
+
 ssh -X $servername
