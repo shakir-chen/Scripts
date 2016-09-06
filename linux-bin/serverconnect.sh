570f433c471401a12408e97771eaaf7ada3fac01
shakir-chen <chenxuanqi93@gmail.com>
2016-09-06 12:43:51 +0800

    up serverconnect

--- linux-bin/serverconnect.sh
+++ linux-bin/serverconnect.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo $1
-if [ "$1" = "lab" ];
+
+if [ "$1" = "lab" ]
 then
     echo "here"
     servername="shakir@143.89.135.225 -p 2222"
+elif [ "$1" = "zhehui" ]
+then
+    servername="zhehui@rostam.ece.ust.hk"
 else
     servername="xuanqi@"$1".ece.ust.hk"
 fi
