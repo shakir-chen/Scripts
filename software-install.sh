17e6cc48471352acd58d8d860175b5d9c1e8d7f1
shakir-chen <chenxuanqi93@gmail.com>
2016-09-23 02:08:49 +0800

    up-new

--- software-install.sh
+++ software-install.sh
@@ -7,16 +7,11 @@ sudo apt-get install subversion
 echo "install tmux"
 sudo apt-get install tmux
 
-echo "install tmux"
-sudo apt-get install tmux
-
 echo "install gparted"
 sudo apt-get install gparted
-
-
 
 #texlive
 echo "install texlive"
 sudo apt-get install texlive
-sudo apt-get install texlive-latex-extra --no-install-recommends            #for a smaller extra package
+sudo apt-get install texlive-latex-extra --no-install-recommends #for a smaller extra package
 
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-ln -s ~/Study/Scripts/mindmap/IT_Stuffs_new.mm ~/
-ln -s ~/Study/Scripts/help-doc/linux-problem.md ~/
+if [[ "$1" == "link" ]]
+then
+    echo "make link"
+    ln -s ~/Study/Scripts/mindmap/IT_Stuffs_new.mm ~/
+    ln -s ~/Study/Scripts/help-doc/linux-problem.md ~/
+    ln -s ~/Study/Scripts/shell/bashrc-ubuntu.sh ~/.bashrc
+    ln -s ~/Study/Scripts/vimperator/_vimperatorrc_linux ~/_vimperatorrc
+    ln -s ~/Study/Scripts/tmux/tmux-folder ~/.tmux
+    ln -s ~/Study/Scripts/tmux/tmux.conf ~/.tmux.conf
+fi
 
-ln -s ~/Study/Scripts/shell/bashrc-ubuntu.sh ~/.bashrc
-ln -s ~/Study/Scripts/vimperator/_vimperatorrc_linux ~/_vimperatorrc
-ln -s ~/Study/Scripts/tmux/tmux-folder ~/.tmux
-ln -s ~/Study/Scripts/tmux/tmux.conf ~/.tmux.conf
+
+if [[ "$1" == "dir" ]]
+then
+    echo "make dir"
+    mkdir ~/Linux
+    mkdir ~/Research
+    cd ~/Research/
+    mkdir Jade
+    mkdir OEIL
+    mkdir Benchmark
+    mkdir Paper
+fi
