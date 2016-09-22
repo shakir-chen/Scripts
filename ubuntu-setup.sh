56808ff4944a763cbd1c398d588e99c5c34cc0c7
shakir-chen <chenxuanqi93@gmail.com>
2016-09-23 00:26:29 +0800

    tmux-modify

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -5,5 +5,5 @@ ln -s ~/Study/Scripts/help-doc/linux-problem.md ~/
 
 ln -s ~/Study/Scripts/shell/bashrc-ubuntu.sh ~/.bashrc
 ln -s ~/Study/Scripts/vimperator/_vimperatorrc_linux ~/_vimperatorrc
-ln -s ~/Study/Scripts/tmux/tmux ~/.tmux
+ln -s ~/Study/Scripts/tmux/tmux-folder ~/.tmux
 ln -s ~/Study/Scripts/tmux/tmux.conf ~/.tmux.conf
