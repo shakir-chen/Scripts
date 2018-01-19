8722a87d0ac36a42a6ccaf431d2e7e1f5333f09f
shakir-chen <chenxuanqi93@gmail.com>
2018-01-19 19:55:13 +0800

    up bashrc

--- shell/bashrc-ubuntu.sh
+++ shell/bashrc-ubuntu.sh
@@ -76,7 +76,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
-
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
@@ -123,7 +122,7 @@ if ! shopt -oq posix; then
   fi
 fi
 
-if [ "$USER" == "shakir" ] ; then
+if [ "$USER" == "shakir" ] ; then           # laptop
     setxkbmap -option caps:ctrl_modifier
 fi
 
@@ -157,12 +156,14 @@ export PYTHON_INCLUDE_DIR=/usr/include/python3.5
 export PYTHON_LIBRARY=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/libpython3.5.so
 
 # export PYTHONPATH=/usr/local/lib/python2.7/site-packages/:${PYTHONPATH}
-# virtualenv and virtualenvwrapper, used for opencv
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.5
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-export OPENCV_TEST_DATA_PATH=~/Software/OpenCV/opencv_extra/testdata
 
+# virtualenv and virtualenvwrapper, used for opencv
+if [ "$HOST" == "lab" ] ; then           # laptop
+    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.5
+    export WORKON_HOME=$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+    export OPENCV_TEST_DATA_PATH=~/Software/OpenCV/opencv_extra/testdata
+fi
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:${PATH}
 
@@ -835,7 +836,9 @@ alias wifid="nmcli c down" # + savedwificonn
 
 
 # autojump
-. /usr/share/autojump/autojump.sh
+if [ "$HOST" == "lab" ] ; then           # laptop
+    source /usr/share/autojump/autojump.sh
+fi
 # https://www.thinkingmedia.ca/2014/10/how-to-install-autojump-on-ubuntu-14/
 
 # mv tmp
